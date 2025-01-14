b8c05c4e1b7ebe3783ebc3bc2376787c6911b5f6
M9heb <123528241+M9heb@users.noreply.github.com>
2025-01-14 15:38:52 +0430

    Edits background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('This is Hi from me!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.cyan],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            // transform: GradientRotation(1.57)),
+          )),
+          child: const Center(
+            child: Text('Hello world!'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
