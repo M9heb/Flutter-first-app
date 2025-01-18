1f914c3afa0a300689f39d8d03f2bc50bad1af3e
M9heb <123528241+M9heb@users.noreply.github.com>
2025-01-18 13:12:48 +0430

    Adds constructor to GradientContainer class

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.cyan],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              // transform: GradientRotation(1.57)),
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.cyan],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          // transform: GradientRotation(1.57)),
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
