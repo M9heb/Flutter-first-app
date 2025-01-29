6dd96a31a8893eb1b74bf5e45cb6a3964136235f
M9heb <123528241+M9heb@users.noreply.github.com>
2025-01-29 14:50:51 +0430

    defines variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:first_project/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.deepPurple, Colors.cyan],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
           // transform: GradientRotation(1.57)),
         ),
       ),
