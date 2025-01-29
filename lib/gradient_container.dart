3f39a040f3b161dbb7e99cfef4c81b0d9a0bbcc2
M9heb <123528241+M9heb@users.noreply.github.com>
2025-01-29 14:58:27 +0430

    applies const keyword on variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:first_project/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -10,7 +10,7 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.deepPurple, Colors.cyan],
           begin: startAlignment,
